e254ff418ed5df5fab8a5c53ddb495abb55046c2
ismail-elshafeiy <ismail.elshafeiy@gmail.com>
2022-06-16 02:34:46 +0200

    #--< Update BrowserFactory.java >--#
    - Add option Headless Execution
    - Add option Remote Execution
    
    Signed-off-by: QC.Ismail Elshafeiy
    <ismail.elshafeiy@gmailcom>

--- src/main/java/utilities/browser/BrowserFactory.java
+++ src/main/java/utilities/browser/BrowserFactory.java
@@ -2,11 +2,13 @@ package utilities.browser;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -20,17 +22,17 @@ import java.net.URL;
 import static org.testng.Assert.fail;
 
 public class BrowserFactory {
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    static DesiredCapabilities capabilities = new DesiredCapabilities();
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     private static final String propertiesFileName = "project.properties";
     private static final String browserTypeProperty = Helper.getProperty(propertiesFileName, "browser.type");
     private static final String executionTypeProperty = Helper.getProperty(propertiesFileName, "execution.type");
-    private static String host = Helper.getProperty(propertiesFileName, "remote.execution.host");
-    private static String port = Helper.getProperty(propertiesFileName, "remote.execution.port");
+    private static final String operatingSystemTypeProperty = Helper.getProperty(propertiesFileName, "targetOperatingSystem");
+    private static final String host = Helper.getProperty(propertiesFileName, "remote.execution.host");
+    private static final String port = Helper.getProperty(propertiesFileName, "remote.execution.port");
 
     public enum BrowserType {
-
         MOZILLA_FIREFOX("Mozilla Firefox"), GOOGLE_CHROME("Google Chrome"), FROM_PROPERTIES(browserTypeProperty);
-
         private final String value;
 
         BrowserType(String type) {
@@ -44,7 +46,6 @@ public class BrowserFactory {
 
     public enum ExecutionType {
         LOCAL("Local"), REMOTE("Remote"), LOCAL_HEADLESS("Local Headless"), FROM_PROPERTIES(executionTypeProperty);
-
         private final String value;
 
         ExecutionType(String type) {
@@ -56,6 +57,35 @@ public class BrowserFactory {
         }
     }
 
+    public enum OperatingSystemType {
+        WINDOWS("Windows-64"), LINUX("Linux-64"), FROM_PROPERTIES(operatingSystemTypeProperty);
+        private final String value;
+
+        OperatingSystemType(String type) {
+            this.value = type;
+        }
+
+        private String getValue() {
+            return value;
+        }
+    }
+
+    private static ChromeOptions getChromeOptions() {
+        ChromeOptions chOptions =
+                new ChromeOptions();
+        chOptions.setHeadless(true);
+        chOptions.addArguments("--window-size=1920,1080");
+        return chOptions;
+    }
+
+    private static FirefoxOptions getFirefoxOptions() {
+        FirefoxOptions ffOptions
+                = new FirefoxOptions();
+        ffOptions.setHeadless(true);
+        ffOptions.addArguments("--window-size=1920,1080");
+        return ffOptions;
+    }
+
 
     /**
      * Check the Browser and Execution from properties file
@@ -63,19 +93,23 @@ public class BrowserFactory {
      * @return BrowserType , ExecutionType
      */
     public static WebDriver getBrowser() {
-        return getBrowser(BrowserType.FROM_PROPERTIES, ExecutionType.FROM_PROPERTIES);
+        return getBrowser(BrowserType.FROM_PROPERTIES, ExecutionType.FROM_PROPERTIES, OperatingSystemType.FROM_PROPERTIES);
     }
+
 
     // Check the Browser and Execution from property file
     @Step("Initializing a new Web GUI Browser!.....")
-    public static WebDriver getBrowser(BrowserType browserType, ExecutionType executionType) {
+    public static WebDriver getBrowser(BrowserType browserType, ExecutionType executionType, OperatingSystemType operatingSystemType) {
         ITestResult result = Reporter.getCurrentTestResult();
         ITestContext context = result.getTestContext();
-        Logger.logStep("Initialize [" + browserType.getValue() + "] Browser and the Execution Type is [" + executionType.getValue() + "]");
+        Logger.logStep("Initialize [" + browserType.getValue() + "] Browser and the Execution Type is [" + executionType.getValue() + "] " + "and the Operating System is [" + operatingSystemType.getValue() + "]");
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////      Remote execution    /////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
         // Remote execution condition
-        if (executionType == ExecutionType.REMOTE
-                || (executionType == ExecutionType.FROM_PROPERTIES && executionTypeProperty.equalsIgnoreCase("remote"))) {
+        if (executionType == ExecutionType.REMOTE || (executionType == ExecutionType.FROM_PROPERTIES && executionTypeProperty.equalsIgnoreCase("remote"))) {
             /*
              * Steps to execute remotely with selenium grid and dockers VERY simple steps:...
              * 1- Install docker
@@ -87,41 +121,95 @@ public class BrowserFactory {
              * 6- open a browser and enter this url to see the grid :D http://localhost:4444/ui/index.html
              * 7- execute using this condition
              */
-            if (browserType == BrowserType.GOOGLE_CHROME
-                    || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("google chrome"))) {
+
+            // Check the browser typeProperty is Chrome >>>>
+            if (browserType == BrowserType.GOOGLE_CHROME || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("google chrome"))) {
 
                 try {
-                    driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"),
-                            getChromeOptions()));
+                    driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getChromeOptions()));
                     context.setAttribute("driver", driver.get());
                     Helper.implicitWait(driver.get());
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
 
+                // Check the operating system type is Linux >>>>
+                if (operatingSystemType == OperatingSystemType.LINUX || (operatingSystemType == OperatingSystemType.FROM_PROPERTIES && operatingSystemTypeProperty.equalsIgnoreCase("Linux-64"))) {
+
+                    try {
+                        driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getChromeOptions()));
+                        capabilities.setPlatform(Platform.LINUX);
+                        Helper.implicitWait(driver.get());
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Check the operating system type is window >>>>
+                } else if (operatingSystemType == OperatingSystemType.WINDOWS || (operatingSystemType == OperatingSystemType.FROM_PROPERTIES && operatingSystemTypeProperty.equalsIgnoreCase("Windows-64"))) {
+                    try {
+                        driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getChromeOptions()));
+                        capabilities.setPlatform(Platform.WINDOWS);
+                        Helper.implicitWait(driver.get());
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    String warningMsg = "The driver is null! because the operatingSystemType  [" + operatingSystemType + "] is not valid, Please choose a valid operatingSystemType from  properties file ";
+                    Logger.logMessage(warningMsg);
+                    fail(warningMsg);
+                }
+
+                // Check the browser typeProperty is firefox  >>>>
             } else if (browserType == BrowserType.MOZILLA_FIREFOX
                     || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("mozilla firefox"))) {
                 try {
-                    driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"),
-                            getFirefoxOptions()));
+                    driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getFirefoxOptions()));
                     context.setAttribute("driver", driver.get());
                     Helper.implicitWait(driver.get());
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
 
+                if (operatingSystemType == OperatingSystemType.LINUX
+                        || (operatingSystemType == OperatingSystemType.FROM_PROPERTIES && operatingSystemTypeProperty.equalsIgnoreCase("Linux-64"))) {
+
+                    try {
+                        driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getFirefoxOptions()));
+                        capabilities.setPlatform(Platform.LINUX);
+                        Helper.implicitWait(driver.get());
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                } else if (operatingSystemType == OperatingSystemType.WINDOWS
+                        || (operatingSystemType == OperatingSystemType.FROM_PROPERTIES && operatingSystemTypeProperty.equalsIgnoreCase("Windows-64"))) {
+                    try {
+                        driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), getFirefoxOptions()));
+                        capabilities.setPlatform(Platform.WINDOWS);
+                        Helper.implicitWait(driver.get());
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    String warningMsg = "The driver is null! because the operatingSystemType  [" + operatingSystemType + "] is not valid, Please choose a valid operatingSystemType from  properties file ";
+                    Logger.logMessage(warningMsg);
+                    fail(warningMsg);
+                }
+
             } else {
-                String warningMsg = "The driver is null! because the browser type [" + browserTypeProperty + "] is not valid/supported; Please choose a valid browser type from the given choices in the properties file";
+                String warningMsg = "The driver is null! because the browser type [" + browserTypeProperty + "] is not valid, Please choose a valid browser type from properties file";
                 Logger.logMessage(warningMsg);
                 fail(warningMsg);
 
             }
+
         }
 
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        /////////  Local execution condition   /////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
-        // Local execution condition
-        else if (executionType == ExecutionType.LOCAL
-                || (executionType == ExecutionType.FROM_PROPERTIES && executionTypeProperty.equalsIgnoreCase("local"))) {
+
+        else if (executionType == ExecutionType.LOCAL || (executionType == ExecutionType.FROM_PROPERTIES && executionTypeProperty.equalsIgnoreCase("local"))) {
 
             if (browserType == BrowserType.GOOGLE_CHROME
                     || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("google chrome"))) {
@@ -130,6 +218,7 @@ public class BrowserFactory {
                 context.setAttribute("driver", driver.get());
                 Helper.implicitWait(driver.get());
 
+
             } else if (browserType == BrowserType.MOZILLA_FIREFOX
                     || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("mozilla firefox"))) {
                 WebDriverManager.firefoxdriver().setup();
@@ -137,15 +226,20 @@ public class BrowserFactory {
                 context.setAttribute("driver", driver.get());
                 Helper.implicitWait(driver.get());
 
+
             } else {
                 String warningMsg = "The driver is null! because the browser type [" + browserTypeProperty + "] is not valid/supported; Please choose a valid browser type from the given choices in the properties file";
                 Logger.logMessage(warningMsg);
                 fail(warningMsg);
             }
+
         }
 
 
-        // Local Headless execution condition
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///////////  Headless execution    /////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
         else if (executionType == ExecutionType.LOCAL_HEADLESS
                 || (executionType == ExecutionType.FROM_PROPERTIES && executionTypeProperty.equalsIgnoreCase("local headless"))) {
 
@@ -180,19 +274,4 @@ public class BrowserFactory {
     }
 
 
-    private static ChromeOptions getChromeOptions() {
-        ChromeOptions chOptions =
-                new ChromeOptions();
-        chOptions.setHeadless(true);
-        chOptions.addArguments("--window-size=1920,1080");
-        return chOptions;
-    }
-
-    private static FirefoxOptions getFirefoxOptions() {
-        FirefoxOptions ffOptions
-                = new FirefoxOptions();
-        ffOptions.setHeadless(true);
-        ffOptions.addArguments("--window-size=1920,1080");
-        return ffOptions;
-    }
 }
